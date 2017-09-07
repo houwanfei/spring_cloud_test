9d140ae0811620ac0d1dd65f5d65888ea06385c1
houwanfei <houwanfei@163.com>
2017-09-07 17:14:58 +0800

    service_ribbon

--- service_ribbon/src/main/java/com/service_ribbon/ServiceRibbonApplication.java
+++ service_ribbon/src/main/java/com/service_ribbon/ServiceRibbonApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
 @Configurable
+@EnableHystrixDashboard
 public class ServiceRibbonApplication {
 
 	public static void main(String[] args) {
