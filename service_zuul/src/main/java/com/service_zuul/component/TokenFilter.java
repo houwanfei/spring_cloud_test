9fd97c922909bc5a6351c9519b009f32861b8e56
houwanfei <houwanfei@163.com>
2017-09-07 19:02:16 +0800

    config-server

---
+++ service_zuul/src/main/java/com/service_zuul/component/TokenFilter.java
@@ -0,0 +1,42 @@
+package com.service_zuul.component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class TokenFilter extends ZuulFilter{
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null){
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(401);
+            try {
+                requestContext.getResponse().getWriter().write("token is empty");
+            } catch (Exception e){
+                return null;
+            }
+        }
+        return null;
+    }
+}
